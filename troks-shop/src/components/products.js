7753ad742f13c5df622ce84c2ed8194023052485
Mike Huffman <ImMikeHuffman@Gmail.com>
2020-01-31 19:46:27 -0800

    Store Framework Working
    Images, Titles, Price, Add To Cart all visible in React App. Images added to App Source,.

---
+++ troks-shop/src/components/products.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react"; 
+import util from "../util";
+
+export default class Products extends Component {
+  render() {
+      const productItems = this.props.products.map( product => (
+        <div className="col-md-4"> 
+            <div className="thumbnail text center">
+                <a href={`#${product.id}`} onClick={(e)=>this.props.handleAddToCard(e, product)}>
+                    <img src={`/products/${product.sku}_.png`} alt={product.title}/>
+                    <p>
+                        {product.title}
+                    </p>
+                </a>
+                <div>
+                    <b>{util.formatCurrency(product.price)}</b>
+                    <button className="btn btn-default"
+                    onClick={(e)=>this.props.handleAddToCard(e, product)}>Add To Cart</button>
+                </div>
+            </div>
+        </div>
+      )
+      )
+    return (
+     <div className="row">
+       {productItems}
+     </div>
+    );
+  }
+}
